89be106acb0b2d878b82e1a34b9e1bd7019b39ae
hpk992 <jkuang41@gmail.com>
2023-10-16 02:24:13 -0700

    feature section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,21 @@
 module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
-        colors: {
-            'dark-blue': '#16425b',
-            primary: '#2f6690',
-            secondary: '#3a7ca5',
-            light: '#81c3d7',
-            black: '#020202',
-            'light-grey': '#d9dcd6',
-            white: '#d9d9d9',
-        },
         fontFamily: {
             mont: ['Montserrat', 'sans-serif'],
             poppins: ['Poppins', 'sans-serif'],
         },
-        extend: {},
+        extend: {
+            colors: {
+                'dark-blue': '#16425b',
+                primary: '#2f6690',
+                secondary: '#3a7ca5',
+                light: '#81c3d7',
+                black: '#020202',
+                'light-grey': '#d9dcd6',
+                white: '#d9d9d9',
+            },
+        },
     },
     plugins: [],
 };
