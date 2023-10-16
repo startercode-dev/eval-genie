33258739bec034ec0983125c5f28fead5628b0d0
hpk992 <jkuang41@gmail.com>
2023-10-15 17:31:17 -0700

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        colors: {
+            'dark-blue': '#16425b',
+            primary: '#2f6690',
+            secondary: '#3a7ca5',
+            light: '#81c3d7',
+            black: '#020202',
+            'light-grey': '#d9dcd6',
+            white: '#d9d9d9',
+        },
+        fontFamily: {
+            mont: ['Montserrat', 'sans-serif'],
+            poppins: ['Poppins', 'sans-serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
