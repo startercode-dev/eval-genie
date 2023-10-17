249e5fecf15ed27797fdc2e46a4b4fbbd8ba6943
hpk992 <jkuang41@gmail.com>
2023-10-17 16:28:04 -0700

    faq js

--- src/js/index.js
+++ src/js/index.js
@@ -2,39 +2,64 @@ import $ from 'jquery';
 
 class ResponsiveNav {
     constructor() {
-        this.nav = document.querySelector('.mobile-nav');
-        this.document = document;
-        this.overlay = document.querySelector('.overlay');
-        this.menuIcon = document.querySelector('.menu');
-        this.closeIcon = document.querySelector('.close');
-        this.header = this.document.querySelector('header');
+        this.nav = $('.mobile-nav');
+        this.document = $(document);
+        this.overlay = $('.overlay');
+        this.menuIcon = $('.menu');
+        this.closeIcon = $('.close');
+        this.header = $('header');
         this.events();
     }
 
     //EVENT
     events() {
-        this.document.addEventListener(
-            'click',
-            this.navMobileMenuClickHandler.bind(this),
-        );
+        this.document.on('click', this.navMobileMenuClickHandler.bind(this));
     }
 
     //MODULES
     navMobileMenuClickHandler(event) {
-        console.log(this.nav);
         if (!!event.target.closest('.mobile-menu')) {
-            this.overlay.classList.toggle('hidden');
-            this.nav.classList.toggle('max-2xl:show');
-            this.menuIcon.classList.toggle('hidden');
-            this.closeIcon.classList.toggle('hidden');
+            this.overlay.toggleClass('hidden');
+            this.nav.toggleClass('max-2xl:show');
+            this.menuIcon.toggleClass('hidden');
+            this.closeIcon.toggleClass('hidden');
         } else {
-            this.overlay.classList.add('hidden');
-            this.nav.classList.remove('max-2xl:show');
-            this.menuIcon.classList.remove('hidden');
-            this.closeIcon.classList.add('hidden');
+            this.overlay.addClass('hidden');
+            this.nav.removeClass('max-2xl:show');
+            this.menuIcon.removeClass('hidden');
+            this.closeIcon.addClass('hidden');
         }
+    }
+}
+
+class Accordion {
+    constructor() {
+        this.accordionItem = $('.accordion-item');
+        this.itemInfo = $('.item-info');
+        this.arrow = $('.caret-icon');
+        this.events();
+    }
+
+    //EVENT
+    events() {
+        this.accordionItem.on('click', this.accordionClickHandler.bind(this));
+    }
+
+    //MODULES
+    accordionClickHandler(event) {
+        const currItem = event.currentTarget;
+
+        this.itemInfo.each((_, item) => {
+            $(item).removeClass('active');
+        });
+        this.arrow.each((_, item) => {
+            $(item).removeClass('caret-up');
+        });
+        $(currItem).find('.item-info').addClass('active');
+        $(currItem).find('.caret-icon').addClass('caret-up');
     }
 }
 $(function () {
     new ResponsiveNav();
+    new Accordion();
 });
