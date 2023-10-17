64bdfb4d9dbf82b2d8039b743c27cd3babd95f27
hpk992 <jkuang41@gmail.com>
2023-10-17 14:30:30 -0700

    responsive nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,40 @@
+import $ from 'jquery';
+
+class ResponsiveNav {
+    constructor() {
+        this.nav = document.querySelector('.mobile-nav');
+        this.document = document;
+        this.overlay = document.querySelector('.overlay');
+        this.menuIcon = document.querySelector('.menu');
+        this.closeIcon = document.querySelector('.close');
+        this.header = this.document.querySelector('header');
+        this.events();
+    }
+
+    //EVENT
+    events() {
+        this.document.addEventListener(
+            'click',
+            this.navMobileMenuClickHandler.bind(this),
+        );
+    }
+
+    //MODULES
+    navMobileMenuClickHandler(event) {
+        console.log(this.nav);
+        if (!!event.target.closest('.mobile-menu')) {
+            this.overlay.classList.toggle('hidden');
+            this.nav.classList.toggle('max-2xl:show');
+            this.menuIcon.classList.toggle('hidden');
+            this.closeIcon.classList.toggle('hidden');
+        } else {
+            this.overlay.classList.add('hidden');
+            this.nav.classList.remove('max-2xl:show');
+            this.menuIcon.classList.remove('hidden');
+            this.closeIcon.classList.add('hidden');
+        }
+    }
+}
 $(function () {
-    // do stuff
+    new ResponsiveNav();
 });
